4e668228fbfa94c4086d1f72203d6d462502a298
Glyn Leine <g.m.leine@gmail.com>
2026-08-16 01:23:00 +0200

    wip

--- src/source.cpp
+++ src/source.cpp
@@ -4,6 +4,8 @@
 #include <rsl/string>
 #include <rsl/threading>
 
+#include <clang-c/Index.h>
+
 int main(int argc, char* argv[])
 {
     rsl::current_thread::set_name("Main thread");
@@ -15,14 +17,17 @@ int main(int argc, char* argv[])
 
     rsl::cli_parser cmdl;
     cmdl.set_command_usage("rrg [options] [<file> ...]\n\noptions:");
-    cmdl.add_param({ "help", "h" }, true, "  --{:<17}\tDisplay help.");
-    cmdl.add_param("version", true, "  --{:<17}\tDisplay version.");
-    cmdl.add_param({ "verbose", "v" }, true, "  --{:<17}\tUse verbose logging.");
-    cmdl.add_param("files", false, "  --{}=<filename>\tA file containing a list of files to process, one per line.");
+    cmdl.add_param({ "help", "h" }, true, "  --{:<21}\tDisplay help.");
+    cmdl.add_param("version", true, "  --{:<21}\tDisplay version.");
+    cmdl.add_param({ "verbose", "v" }, true, "  --{:<21}\tUse verbose logging.");
+    cmdl.add_param("files", false, "  --{}=<filename>\t\tA file containing a list of files to process, one per line.");
+    cmdl.add_param("pch", false, "  --{}=<filename>\t\tUse a precompiled header to speed up process.");
+    cmdl.add_param("intermediates", false, "  --{}=<path>\tUse an intermediates folder to allow partial recompilation of changed files only.");
+    cmdl.add_param("i", false, "  --{}=<path>\t\t\tUse an intermediates folder to allow partial recompilation of changed files only.");
 
     cmdl.parse(argc, argv);
 
-    if (cmdl.has_flag({ "help", "h" }) || cmdl.is_empty())
+    if (cmdl.has_flag({ "help", "h" }) || cmdl.is_empty() || true)
     {
         cmdl.print_usage();
         return 0;
@@ -34,17 +39,54 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    if (cmdl.has_flag({ "verbose", "v" }))
+    const bool verbose = cmdl.has_flag({ "verbose", "v" });
+
+    if (verbose)
     {
         rsl::log::filter(rsl::log::severity::trace);
     }
 
-    rsl::dynamic_set<rsl::string_view> files;
+    // lazy deduplicate
+    rsl::dynamic_array<rsl::string_view> files =
+            rsl::dynamic_array<rsl::string_view>::from_view(rsl::dynamic_set<rsl::string_view>::from_view(cmdl.pos_args()).view());
 
-    for (auto& param : cmdl.pos_args())
+    // lazy deduplicate
+    rsl::dynamic_array<rsl::string_view> pchFiles =
+            rsl::dynamic_array<rsl::string_view>::from_view(rsl::dynamic_set<rsl::string_view>::from_view(cmdl.get_params("pch")).view());
+
+    CXIndex index = clang_createIndex(!pchFiles.is_empty(), verbose);
+
+    int translationUnitFlags = CXTranslationUnit_Incomplete | CXTranslationUnit_SkipFunctionBodies | CXTranslationUnit_KeepGoing |
+            CXTranslationUnit_SingleFileParse | CXTranslationUnit_IncludeAttributedTypes |
+            CXTranslationUnit_IgnoreNonErrorsFromIncludedFiles;
+
+    if (pchFiles.is_empty())
     {
-        rsl::log::debug("{}", param);
-        files.insert(param);
+        translationUnitFlags |= CXTranslationUnit_PrecompiledPreamble | CXTranslationUnit_CreatePreambleOnFirstParse;
+    }
+
+    for (auto& pch : pchFiles)
+    {
+        CXTranslationUnit translationUnit;
+        CXErrorCode error = clang_createTranslationUnit2(index, pch.data(), &translationUnit);
+        if (error != CXError_Success)
+        {
+            rsl::log::error("failed to parse pch file \"{}\", pch will be ignored", pch);
+        }
+        clang_disposeTranslationUnit(translationUnit);
+    }
+
+    for (auto& file : files)
+    {
+        CXTranslationUnit translationUnit;
+        CXErrorCode error =
+                clang_parseTranslationUnit2(index, file.data(), nullptr, 0, nullptr, 0, translationUnitFlags, &translationUnit);
+        if (error != CXError_Success)
+        {
+            rsl::log::error("failed to parse source file: \"{}\"", file);
+            return error;
+        }
+        clang_disposeTranslationUnit(translationUnit);
     }
 
     return 0;
